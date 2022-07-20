c17670052b539fba0708071e534a028a76696127
jimas <lqj1110@sina.cn>
2022-07-20 23:25:14 +0800

    ckh

---
+++ src/main/java/com/janet/classloader/A.java
@@ -0,0 +1,13 @@
+package com.janet.classloader;
+
+/**
+ * Date: 2022/7/20
+ * Time: 23:19
+ *
+ * @author jimas
+ */
+public class A {
+    public static void main(String[] args) {
+        A a = new A();
+    }
+}
